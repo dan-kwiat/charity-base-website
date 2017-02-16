2ab122becc0137b73fa0e72873461d516ba1c10d
Dan <dan.kwiat@gmail.com>
2017-02-16 18:04:43 +0000

    Add nav tabs using react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Nav, NavItem } from 'react-bootstrap';
+
 
 class App extends Component {
+  state = {
+    activeKey: 1
+  }
+  handleSelect = (selectedKey) => {
+    this.setState({
+      activeKey: selectedKey
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +23,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="container">
+          <Nav bsStyle="tabs" activeKey={this.state.activeKey} onSelect={this.handleSelect}>
+            <NavItem eventKey={1}>One</NavItem>
+            <NavItem eventKey={2}>Two</NavItem>
+            <NavItem eventKey={3}>Three</NavItem>
+          </Nav>
+        </div>
       </div>
     );
   }
