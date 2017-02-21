1b0474a5fd94e187879bf71e228347b60f42a9c6
Dan <dan.kwiat@gmail.com>
2017-02-21 15:10:37 +0000

    Define Filter component

--- src/components/Charities.js
+++ src/components/Charities.js
@@ -1,5 +1,63 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
+import { Form, FormGroup, Col, ControlLabel, FormControl, Checkbox } from 'react-bootstrap';
+
+const Filters = () => {
+  return (
+    <div className="filter-form">
+      <h3>Filters</h3>
+      <Form horizontal>
+        <FormGroup>
+          <Col componentClass={ControlLabel} sm={6}>
+            Minimum Gross Income
+          </Col>
+          <Col sm={6}>
+            <FormControl type="text" placeholder="E.g. 0" />
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Col componentClass={ControlLabel} sm={6}>
+            Maximum Gross Income
+          </Col>
+          <Col sm={6}>
+            <FormControl type="text" placeholder="E.g. 17000" />
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Col componentClass={ControlLabel} sm={6}>
+            Search terms
+          </Col>
+          <Col sm={6}>
+            <FormControl type="text" placeholder="E.g. NHS London" />
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Col componentClass={ControlLabel} sm={6}>
+            Charity Number
+          </Col>
+          <Col sm={6}>
+            <FormControl type="text" placeholder="E.g. 202918" />
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Col componentClass={ControlLabel} sm={6}>
+            Subsidiary Number
+          </Col>
+          <Col sm={6}>
+            <FormControl type="text" placeholder="E.g. 0" />
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Col sm={12}>
+            <Checkbox>Registered charities only</Checkbox>
+          </Col>
+        </FormGroup>
+      </Form>
+    </div>
+  )
+}
+
+
 
 
 export class Charities extends Component {
@@ -9,6 +67,10 @@ export class Charities extends Component {
   render() {
     return (
       <div>
+        <div className="row">
+          <div className="col-xs-6"><Filters /></div>
+          <div className="col-xs-6"></div>
+        </div>
         <input />
         {this.state.charities.map((x,i) => {
           return (
