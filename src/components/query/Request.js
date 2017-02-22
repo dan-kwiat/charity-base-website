66dcf68d8116534516c8010003e1ec2d91c26593
Dan <dan.kwiat@gmail.com>
2017-02-22 14:22:54 +0000

    Include ? in request query string

--- src/components/query/Request.js
+++ src/components/query/Request.js
@@ -5,7 +5,7 @@ export const Request = ({filter, projection}) => {
   return (
     <Panel collapsible defaultExpanded header="GET Request">
       <pre className="query-string">
-        <span className="default-query">https://charitybase.uk/api/v0.2.0/charities/</span>
+        <span className="default-query">https://charitybase.uk/api/v0.2.0/charities/?</span>
         <span className="filter-query">{filter}</span>
         <span className="default-query">&</span>
         <span className="projection-query">{projection}</span>
