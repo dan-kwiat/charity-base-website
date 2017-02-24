138d0a938c0d3ab144e6ba25d23a1b8934c53c1c
Dan <dan.kwiat@gmail.com>
2017-02-24 17:31:45 +0000

    Add page number to QueryBuilder

--- src/components/query/Pages.js
+++ src/components/query/Pages.js
@@ -1,27 +1,42 @@
 import React, {Component} from 'react';
-import { Col, FormGroup, Checkbox } from 'react-bootstrap';
+import { Col, FormGroup, Checkbox, ControlLabel, FormControl } from 'react-bootstrap';
 
 export class Pages extends Component {
   state = {
-    countResults: false
+    countResults: false,
+    limit: 10,
+    skip: 0
   }
   componentDidMount() {
-    this.updateString(this.state.countResults)
+    this.updateString(this.state.countResults, this.state.skip)
   }
   componentDidUpdate(prevProps, prevState) {
     const stateUpdated = prevState!==this.state
     if (stateUpdated) {
-      this.updateString(this.state.countResults)
+      this.updateString(this.state.countResults, this.state.skip)
     }
   }
-  updateString = (countResults) => {
-    const pageString = countResults ? 'countResults' : ''
+  updateString = (countResults, skip) => {
+    const pageString = `skip=${skip}` + (countResults ? '&countResults' : '')
     this.props.onChange(pageString)
   }
   render() {
     return (
       <div className="query-box page-form">
         <h3 className="text-center">Pagination</h3>
+        <FormGroup>
+          <Col componentClass={ControlLabel} sm={4}>
+            Page
+          </Col>
+          <Col sm={8}>
+            <FormControl
+            type="number"
+            placeholder="E.g. 1"
+            defaultValue={1 + this.state.skip/10}
+            onChange={e => this.setState({skip: this.state.limit*(e.target.value-1)})}
+            />
+          </Col>
+        </FormGroup>
         <FormGroup>
           <Col xs={12}>
             <Checkbox
