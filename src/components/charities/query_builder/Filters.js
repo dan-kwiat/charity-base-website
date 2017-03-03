638dddccb0b3448cbf1f8a6b79819c3d662f0e8c
Dan <dan.kwiat@gmail.com>
2017-03-03 16:53:18 +0000

    Stack filter inputs in responsive columns

--- src/components/charities/query_builder/Filters.js
+++ src/components/charities/query_builder/Filters.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FormGroup, Col, ControlLabel, FormControl, Checkbox } from 'react-bootstrap';
+import { FormGroup, Row, Col, ControlLabel, FormControl, Checkbox } from 'react-bootstrap';
 import { removeByValue } from '../../../lib/queryBuilderHelpers';
 
 
@@ -12,31 +12,37 @@ export class Filters extends Component {
     this.props.onChange(pruned)
   }
   inputs = [
-    {name: 'Search Terms', key: 'search=', type: 'text', placeHolder: 'E.g. NHS London'},
     {name: 'Minimum Gross Income', key: 'mainCharity.income>=', type: 'number', placeHolder: 'E.g. 0'},
     {name: 'Maximum Gross Income', key: 'mainCharity.income<=', type: 'number', placeHolder: 'E.g. 17000'},
     {name: 'Charity Number', key: 'charityNumber=', type: 'number', placeHolder: 'E.g. 202918', min: 200000},
-    {name: 'Subsidiary Number', key: 'subNumber=', type: 'number', placeHolder: 'E.g. 0', min: 0}
+    {name: 'Subsidiary Number', key: 'subNumber=', type: 'number', placeHolder: 'E.g. 0', min: 0},
+    {name: 'Search Terms', key: 'search=', type: 'text', placeHolder: 'E.g. NHS London'}
   ]
   render() {
     const {query} = this.props
     return (
       <div className="query-box filter-form">
-        {this.inputs.map((x, j) => (
-          <FormGroup key={j}>
-            <Col componentClass={ControlLabel} sm={6}>
-              {x.name}
+        <Row>
+          {this.inputs.map((x, j) => (
+            <Col key={j} sm={6}>
+              <FormGroup key={j}>
+                <Col componentClass={ControlLabel} sm={6}>
+                  {x.name}
+                </Col>
+                <Col sm={6}>
+                  <FormControl type={x.type} min={x.min} placeholder={x.placeHolder} defaultValue={query[x.key]} onChange={this.updateQuery.bind(null, x.key)} />
+                </Col>
+              </FormGroup>
             </Col>
-            <Col sm={6}>
-              <FormControl type={x.type} min={x.min} placeholder={x.placeHolder} defaultValue={query[x.key]} onChange={this.updateQuery.bind(null, x.key)} />
-            </Col>
-          </FormGroup>
-        ))}
-        <FormGroup>
-          <Col smOffset={6} sm={6}>
-            <Checkbox defaultChecked={query['registered=']} onChange={this.updateQuery.bind(null, 'registered=')} >Registered / De-registered</Checkbox>
+          ))}
+          <Col sm={6}>
+            <FormGroup>
+              <Col xs={12} className="text-center">
+                <Checkbox defaultChecked={query['registered=']} onChange={this.updateQuery.bind(null, 'registered=')} >Registered / De-registered</Checkbox>
+              </Col>
+            </FormGroup>
           </Col>
-        </FormGroup>
+        </Row>
       </div>
     )
   }
