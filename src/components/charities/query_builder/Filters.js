1c5e5d5add60381b47fd8835ddf5975f8624db8b
Dan <dan.kwiat@gmail.com>
2017-03-03 16:32:04 +0000

    Put filter input forms in an array

--- src/components/charities/query_builder/Filters.js
+++ src/components/charities/query_builder/Filters.js
@@ -10,51 +10,28 @@ export class Filters extends Component {
     const pruned = removeByValue(query, '')
     this.props.onChange(pruned)
   }
+  inputs = [
+    {name: 'Search Terms', key: 'search=', type: 'text', placeHolder: 'E.g. NHS London'},
+    {name: 'Minimum Gross Income', key: 'mainCharity.income>=', type: 'number', placeHolder: 'E.g. 0'},
+    {name: 'Maximum Gross Income', key: 'mainCharity.income<=', type: 'number', placeHolder: 'E.g. 17000'},
+    {name: 'Charity Number', key: 'charityNumber=', type: 'number', placeHolder: 'E.g. 202918', min: 200000},
+    {name: 'Subsidiary Number', key: 'subNumber=', type: 'number', placeHolder: 'E.g. 0', min: 0}
+  ]
   render() {
     const {query} = this.props
     return (
       <div className="query-box filter-form">
         <h3 className="text-center">Filters</h3>
-        <FormGroup>
-          <Col componentClass={ControlLabel} sm={6}>
-            Search terms
-          </Col>
-          <Col sm={6}>
-            <FormControl type="text" placeholder="E.g. NHS London" defaultValue={query['search=']} onChange={this.updateQuery.bind(null, 'search=')} />
-          </Col>
-        </FormGroup>
-        <FormGroup>
-          <Col componentClass={ControlLabel} sm={6}>
-            Minimum Gross Income
-          </Col>
-          <Col sm={6}>
-            <FormControl type="number" placeholder="E.g. 0" defaultValue={query['mainCharity.income>=']} onChange={this.updateQuery.bind(null, 'mainCharity.income>=')} />
-          </Col>
-        </FormGroup>
-        <FormGroup>
-          <Col componentClass={ControlLabel} sm={6}>
-            Maximum Gross Income
-          </Col>
-          <Col sm={6}>
-            <FormControl type="number" placeholder="E.g. 17000" defaultValue={query['mainCharity.income<=']} onChange={this.updateQuery.bind(null, 'mainCharity.income<=')} />
-          </Col>
-        </FormGroup>
-        <FormGroup>
-          <Col componentClass={ControlLabel} sm={6}>
-            Charity Number
-          </Col>
-          <Col sm={6}>
-            <FormControl type="number" min={200000} placeholder="E.g. 202918" defaultValue={query['charityNumber=']} onChange={this.updateQuery.bind(null, 'charityNumber=')} />
-          </Col>
-        </FormGroup>
-        <FormGroup>
-          <Col componentClass={ControlLabel} sm={6}>
-            Subsidiary Number
-          </Col>
-          <Col sm={6}>
-            <FormControl type="number" min={0} placeholder="E.g. 0" defaultValue={query['subNumber=']} onChange={this.updateQuery.bind(null, 'subNumber=')} />
-          </Col>
-        </FormGroup>
+        {this.inputs.map((x, j) => (
+          <FormGroup key={j}>
+            <Col componentClass={ControlLabel} sm={6}>
+              {x.name}
+            </Col>
+            <Col sm={6}>
+              <FormControl type={x.type} min={x.min} placeholder={x.placeHolder} defaultValue={query[x.key]} onChange={this.updateQuery.bind(null, x.key)} />
+            </Col>
+          </FormGroup>
+        ))}
         <FormGroup>
           <Col smOffset={6} sm={6}>
             <Checkbox defaultChecked={query['registered=']} onChange={this.updateQuery.bind(null, 'registered=')} >Registered / De-registered</Checkbox>
